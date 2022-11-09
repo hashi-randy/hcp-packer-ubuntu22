cb8dfd4c2159740ceb9fefa2ce21cbca03fef189
Dan Barr <dan@dbarr.com>
2022-11-09 18:32:53 -0500

    Add debconf setting

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt-get -qy update && 
     apt-get -qy -o "Dpkg::Options::=--force-confdef" -o "Dpkg::Options::=--force-confold" dist-upgrade &&
     apt-get -qy clean
